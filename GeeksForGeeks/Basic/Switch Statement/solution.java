da14b5b17f0f0dcb8c48d235de5a752930ea1047
samirkumar467 <sameerkupradhan705@gmail.com>
2026-08-03 12:15:56 +0530

    [Syncode] GeeksForGeeks/Basic/Switch Statement

---
+++ GeeksForGeeks/Basic/Switch Statement/solution.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+        switch(n){
+            case 1:
+                if(n==1){
+                    System.out.print("One");
+                }
+                break;
+                case 2:
+                if(n==2){
+                    System.out.print("Two");
+                }
+                break;
+                case 3:
+                if(n==3){
+                    System.out.print("Three");
+                }
+                break;
+                case 4:
+                if(n==4){
+                    System.out.print("Four");
+                }
+                break;
+                case 5:
+                if(n==5){
+                    System.out.print("Five");
+                }
+                break;
+                case 6:
+                if(n==6){
+                    System.out.print("Six");
+                }
+                break;
+                case 7:
+                if(n==7){
+                    System.out.print("Seven");
+                }
+                break;
+                 case 8:
+                if(n==8){
+                    System.out.print("Eight");
+                }
+                break;
+                 case 9:
+                if(n==9){
+                    System.out.print("Nine");
+                }
+                break;
+                
+                default:
+                System.out.print("Unknown");
+                
+        }
+        
+    }
+}
--- README.md
+++ README.md
@@ -17,3 +17,4 @@
 | 15 | [Print Square](./GeeksForGeeks/Basic/Print%20Square) | [GeeksForGeeks](https://www.geeksforgeeks.org/problems/print-square--105330/1?page=1&category=Java&sortBy=submissions) | Basic | 01 Aug 2026 | 10:54 pm |
 | 16 | [Print Solid Square](./GeeksForGeeks/Medium/Print%20Solid%20Square) | [GeeksForGeeks](https://www.geeksforgeeks.org/problems/print-square-wall-1605682270/1?page=1&category=Java&sortBy=submissions) | Medium | 01 Aug 2026 | 11:05 pm |
 | 17 | [Print With Separator](./GeeksForGeeks/Basic/Print%20With%20Separator) | [GeeksForGeeks](https://www.geeksforgeeks.org/problems/print-with-separator/1?page=1&category=Java&sortBy=submissions) | Basic | 03 Aug 2026 | 12:09 pm |
+| 18 | [Switch Statement](./GeeksForGeeks/Basic/Switch%20Statement) | [GeeksForGeeks](https://www.geeksforgeeks.org/problems/switch-statement/1?page=2&category=Java&sortBy=submissions) | Basic | 03 Aug 2026 | 12:15 pm |
